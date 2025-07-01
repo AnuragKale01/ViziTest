3073d4db6115179e60a97092ed9373778c909d67
Anurag kale <anukaley09@gmail.com>
2025-07-01 13:01:50 +0530

    BaseTest Commit

--- src/test/java/AutomationTesting/Vizismart/DownloadFile.java
+++ src/test/java/AutomationTesting/Vizismart/DownloadFile.java
@@ -3,68 +3,65 @@ package AutomationTesting.Vizismart;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-import com.github.dockerjava.api.model.Driver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class DownloadFile extends BaseTest {
-	@Test
-	public void login_page() throws InterruptedException {
-//		WebDriverManager.chromedriver().setup();
-//		ChromeDriver driver = new ChromeDriver();
-		Reporter.log("ChromeDriver initialized", true);
 
-		driver.get("https://dev.vizismart.com/login");
-		Reporter.log("Navigated to Login page", true);
+    @Test
+    public void login_and_download() throws InterruptedException {
+        Reporter.log("Starting DownloadFile test", true);
 
-		driver.manage().window().maximize();
+        driver.get("https://dev.vizismart.com/login");
+        Reporter.log("Navigated to Login page", true);
 
-		WebElement emailField = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div/form/input"));
-		emailField.sendKeys("kalyanideshmukh778+5@gmail.com");
-		Reporter.log("Entered Email", true);
+        // Login
+        WebElement emailField = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/input"));
+        emailField.sendKeys("kalyanideshmukh778+5@gmail.com");
+        Reporter.log("Entered email", true);
 
-		WebElement password = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div/form/div/input"));
-		password.sendKeys("12345678");
-		Reporter.log("Entered Password", true);
+        WebElement password = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/div/input"));
+        password.sendKeys("12345678");
+        Reporter.log("Entered password", true);
 
-		WebElement Login = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
-		Login.click();
-		Reporter.log("Clicked on Login button", true);
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginBtn.click();
+        Reporter.log("Clicked on Login button", true);
 
-		Thread.sleep(2000);
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-//        
-		wait.until(ExpectedConditions.urlContains("https://dev.vizismart.com/properties"));
+        // Wait until redirected to the properties page
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.urlContains("/properties"));
+        Reporter.log("Landed on Properties page", true);
 
-//        
-		WebElement svgIcon = wait.until(ExpectedConditions.elementToBeClickable(
-				By.xpath("//*[name()='svg' and contains(@class, 'scale-150') and contains(@class, 'text-white')]")));
-		svgIcon.click();
+        // Open the option menu (SVG icon)
+        WebElement svgIcon = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//*[name()='svg' and contains(@class,'scale-150') and contains(@class,'text-white')]")
+        ));
+        svgIcon.click();
+        Reporter.log("Clicked on Option Menu SVG icon", true);
 
-		Thread.sleep(5000);
+        // Click "Manage KBase" (adjust XPath if necessary)
+        WebElement manageKBase = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//*[@id='root']/div/div[1]/div/div[3]/div/div[2]")
+        ));
+        manageKBase.click();
+        Reporter.log("Clicked on Manage KBase button", true);
 
-		Reporter.log("Clicked on Option Menu", true);
+        // Wait for table row and click the download link in the first row
+        WebElement downloadLink = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//*[@id='root']/div/div[3]/div/div/div/div[1]/table/tbody/tr[1]/td[4]/a")
+        ));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", downloadLink);
+        downloadLink.click();
+        Reporter.log("Clicked download link on first row", true);
 
-		WebElement ManageKBaseButton = wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div[3]/div/div[2]")));
-		ManageKBaseButton.click();
-		Thread.sleep(5000);
-		Reporter.log("Option Menu Clicked", true);
-		
-		WebElement DownloadButton=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div/div[1]/table/tbody/tr[1]/td[4]/a"));
-		DownloadButton.click();
-		
-		Thread.sleep(2000);
-		
-		driver.quit();
-		Reporter.log("Closed the browser", true);
-	}
+        // Optionally, wait until download completes or some success indicator appears
+        // e.g., wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("download-success-alert")));
+
+        // Let the BaseTest @AfterMethod handle driver.quit()
+    }
 }
