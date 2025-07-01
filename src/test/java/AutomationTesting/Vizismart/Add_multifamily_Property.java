1f9986d254aa5ed97415a85b3c1ae7fac4e62e6b
Anurag kale <anukaley09@gmail.com>
2025-07-01 14:51:30 +0530

    some slector related changes

--- src/test/java/AutomationTesting/Vizismart/Add_multifamily_Property.java
+++ src/test/java/AutomationTesting/Vizismart/Add_multifamily_Property.java
@@ -5,97 +5,94 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
- 
-import io.github.bonigarcia.wdm.WebDriverManager;
- 
+
 public class Add_multifamily_Property extends BaseTest {
-	
-	@Test
-	public void add() throws InterruptedException  {
-//        WebDriverManager.chromedriver().setup();
-//        ChromeDriver driver = new ChromeDriver();
-        Reporter.log("ChromeDriver initialized", true);
- 
+
+    @Test
+    public void add() throws InterruptedException {
+        Reporter.log("Starting Add_multifamily_Property test", true);
+
         driver.get("https://dev.vizismart.com/login");
         Reporter.log("Navigated to Login page", true);
-        
-        driver.manage().window().maximize();
-        
-        // Login as property manager process ...
-        WebElement emailField = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div/form/input"));
+
+        // Login
+        WebElement emailField = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/input")); // ✅ RECOMMENDED: Use CSS selector like input[type='email']
         emailField.sendKeys("kalyanideshmukh778+11@gmail.com");
         Reporter.log("Entered email address", true);
- 
-        WebElement password = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div/form/div/input"));
+
+        WebElement password = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/div/input")); // ✅ RECOMMENDED: Use input[type='password'] as CSS selector
         password.sendKeys("12345678");
-        Reporter.log("Entered Password", true);
-        
-        WebElement Login = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
-        Login.click();
+        Reporter.log("Entered password", true);
+
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']")); // ✅ RECOMMENDED: CSS selector also works: button[type='submit']
+        loginBtn.click();
         Reporter.log("Clicked on Login button", true);
-        
-        // Add multi family process ...
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement Property_icon = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"Properties6\"]")));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Property_icon);
-        Property_icon.click();
+
+        // Click on Property icon
+        WebElement propertyIcon = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//*[@id='Properties6']"))); // ✅ RECOMMENDED: Use By.id("Properties6") directly
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", propertyIcon);
+        propertyIcon.click();
         Reporter.log("Clicked on Property icon", true);
-        
-        WebElement MultiFamily =driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/form/div[1]/div/div/div[2]/label/input"));
-        MultiFamily.click();
-        Reporter.log("Click on multi-familty property", true);
-        
-        WebElement Address = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/form/div[1]/input[1]"));
-        // Scroll to the Address field
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Address);
-        Address.sendKeys("Five Garden plaza");
-        Reporter.log("Entered the address.", true);
-        
-        WebElement City = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/form/div[1]/input[2]"));
-        // Scroll to the City field
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", City);
-        City.sendKeys("Pune");
-        Reporter.log("Entered the city.", true);
-        
-        WebElement State = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/form/div[1]/input[3]"));
-        // Scroll to the State field
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", State);
-        State.sendKeys("Maharashtra");
-        Reporter.log("Entered the state.", true);
-        
-        WebElement Zipcode = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/form/div[1]/input[4]"));
-        // Scroll to the Zipcode field
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Zipcode);
-        Zipcode.sendKeys("411057");
-        Reporter.log("Entered the zip code.", true);
- 
-        
-        //add units...
-        WebElement unit1= driver.findElement(By.xpath("//*[@id=\"add14\"]"));
+
+        // MultiFamily radio button
+        WebElement multiFamily = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//*[@id=\"Multi Family\"]"))); // ✅ RECOMMENDED: Use relative XPath or label-based XPath
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", multiFamily);
+        try {
+            multiFamily.click();
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", multiFamily);
+        }
+        Reporter.log("Clicked on Multi-Family property", true);
+
+        // Address fields
+        WebElement address = driver.findElement(By.xpath("//*[@id='root']/div/div[3]/div/div/form/div[1]/input[1]")); // ✅ RECOMMENDED: Use placeholder or name attribute as CSS selector
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", address);
+        address.sendKeys("Five Garden plaza");
+        Reporter.log("Entered address", true);
+
+        WebElement city = driver.findElement(By.xpath("//*[@id='root']/div/div[3]/div/div/form/div[1]/input[2]")); // ✅ RECOMMENDED: Same as above
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", city);
+        city.sendKeys("Pune");
+        Reporter.log("Entered city", true);
+
+        WebElement state = driver.findElement(By.xpath("//*[@id='root']/div/div[3]/div/div/form/div[1]/input[3]")); // ✅ RECOMMENDED: Same as above
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", state);
+        state.sendKeys("Maharashtra");
+        Reporter.log("Entered state", true);
+
+        WebElement zip = driver.findElement(By.xpath("//*[@id='root']/div/div[3]/div/div/form/div[1]/input[4]")); // ✅ RECOMMENDED: Same as above
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", zip);
+        zip.sendKeys("411057");
+        Reporter.log("Entered zip code", true);
+
+        // Add units
+        WebElement unit1 = driver.findElement(By.xpath("//*[@id='add14']")); // ✅ RECOMMENDED: Use By.id("add14")
         unit1.sendKeys("1");
-        WebElement addunits = driver.findElement(By.xpath("//*[@id=\"add16\"]"));
-        addunits.click();
-        WebElement unit2= driver.findElement(By.xpath("/html/body/div/div/div[3]/div/div/form/div[1]/div[2]/div[2]/input"));
+
+        WebElement addUnitsBtn = driver.findElement(By.xpath("//*[@id='add16']")); // ✅ RECOMMENDED: Use By.id("add16")
+        addUnitsBtn.click();
+
+        WebElement unit2 = driver.findElement(
+            By.xpath("/html/body/div/div/div[3]/div/div/form/div[1]/div[2]/div[2]/input")); // ✅ RECOMMENDED: Use placeholder='Unit #' or class if available
         unit2.sendKeys("2");
-        
-        
-        WebElement SaveBtn = driver.findElement(By.xpath("//*[@id=\"add23\"]"));
-        // Scroll to the Save button
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", SaveBtn);
-        SaveBtn.click();
-        Reporter.log("Clicked on save button", true);
-        
-  
-        Thread.sleep(2000);
-        
-        driver.close();
-        
-	}
- 
+        Reporter.log("Added 2 units", true);
+
+        // Save
+        WebElement saveBtn = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//*[@id='add23']"))); // ✅ RECOMMENDED: Use By.id("add23")
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", saveBtn);
+        saveBtn.click();
+        Reporter.log("Clicked on Save button", true);
+
+        // Final pause
+        Thread.sleep(2000); // ✅ Consider replacing with explicit wait
+    }
 }
- 
--- src/test/java/AutomationTesting/Vizismart/DownloadFile.java
+++ src/test/java/AutomationTesting/Vizismart/DownloadFile.java
@@ -1,7 +1,6 @@
 package AutomationTesting.Vizismart;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -20,48 +19,46 @@ public class DownloadFile extends BaseTest {
         Reporter.log("Navigated to Login page", true);
 
         // Login
-        WebElement emailField = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/input"));
+        // WebElement emailField = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/input"));
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.sendKeys("kalyanideshmukh778+5@gmail.com");
         Reporter.log("Entered email", true);
 
-        WebElement password = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/div/input"));
+        // WebElement password = driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div/div/form/div/input"));
+        WebElement password = driver.findElement(By.cssSelector("input[type='password']"));
         password.sendKeys("12345678");
         Reporter.log("Entered password", true);
 
-        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        // WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
         loginBtn.click();
         Reporter.log("Clicked on Login button", true);
 
-        // Wait until redirected to the properties page
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.urlContains("/properties"));
         Reporter.log("Landed on Properties page", true);
 
-        // Open the option menu (SVG icon)
+        // SVG icon for option menu
+        // WebElement svgIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[name()='svg' and contains(@class,'scale-150') and contains(@class,'text-white')]")));
         WebElement svgIcon = wait.until(ExpectedConditions.elementToBeClickable(
-            By.xpath("//*[name()='svg' and contains(@class,'scale-150') and contains(@class,'text-white')]")
-        ));
+            By.cssSelector("svg.scale-150.text-white")));
         svgIcon.click();
         Reporter.log("Clicked on Option Menu SVG icon", true);
 
-        // Click "Manage KBase" (adjust XPath if necessary)
+        // WebElement manageKBase = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[1]/div/div[3]/div/div[2]")));
         WebElement manageKBase = wait.until(ExpectedConditions.elementToBeClickable(
-            By.xpath("//*[@id='root']/div/div[1]/div/div[3]/div/div[2]")
-        ));
+            By.xpath("//div[text()='Manage KBase']"))); // Better than full path if text is unique
         manageKBase.click();
         Reporter.log("Clicked on Manage KBase button", true);
 
-        // Wait for table row and click the download link in the first row
+        // WebElement downloadLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[3]/div/div/div/div[1]/table/tbody/tr[1]/td[4]/a")));
         WebElement downloadLink = wait.until(ExpectedConditions.elementToBeClickable(
-            By.xpath("//*[@id='root']/div/div[3]/div/div/div/div[1]/table/tbody/tr[1]/td[4]/a")
-        ));
+            By.cssSelector("table tbody tr:first-child td:nth-child(4) a")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", downloadLink);
         downloadLink.click();
         Reporter.log("Clicked download link on first row", true);
 
-        // Optionally, wait until download completes or some success indicator appears
-        // e.g., wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("download-success-alert")));
-
-        // Let the BaseTest @AfterMethod handle driver.quit()
+        // Optional: wait for download completion indicator if available
+        // wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("download-success-alert")));
     }
 }
